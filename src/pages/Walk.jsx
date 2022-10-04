f202e001cae8984417ad78faa3e3967625001f1e
Vinicius Pietscher <8095975+viniciuspietscher@users.noreply.github.com>
2022-10-04 18:05:35 -0400

    add walk form

--- src/pages/Walk.jsx
+++ src/pages/Walk.jsx
@@ -1,24 +1,121 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  Grid,
+  Group,
+  NumberInput,
+  Textarea,
+} from "@mantine/core"
+import { useForm } from "@mantine/form"
 import axios from "axios"
-import { useEffect } from "react"
+import { useState } from "react"
 import { useParams } from "react-router-dom"
 
 export function Walk() {
   const { eventId } = useParams()
+  const [link, setLink] = useState("")
+  const form = useForm({
+    initialValues: {
+      petSittingEvent: eventId,
+      date: Date(),
+      starttime: Date(),
+      endtime: "",
+      walknotes: "",
+      pee: 0,
+      poop: 0,
+      food: false,
+      water: false,
+      medicine: false,
+      pictures: [],
+    },
+  })
 
-  const getPetSittingEvent = (id) => {
-    axios
-      .get(`${process.env.REACT_APP_BASEURL}/petsitting/petSitting/${id}`, {
-        headers: { Authorization: `Bearer ${localStorage.token}` },
-      })
-      .then((response) => {
-        // console.log(response.data)
-      })
-      .catch((e) => console.log("error", e))
+  const handleSubmit = () => {
+    form.values.endtime = Date()
+    console.log(form.values)
   }
 
-  useEffect(() => {
-    getPetSittingEvent(eventId)
-  }, [])
-
-  return <div>{eventId}</div>
+  return (
+    <>
+      <Container mt={50}>
+        <Group position='center'>
+          <h2>New Walk</h2>
+        </Group>
+      </Container>
+      <Container mt={50}>
+        <Grid>
+          <Grid.Col xs={12}>
+            <Box sx={{ maxWidth: 500 }} mx='auto'>
+              <form onSubmit={form.onSubmit(handleSubmit)}>
+                <Group spacing={5}>
+                  <NumberInput
+                    placeholder='Number of times pet peed'
+                    label='Pee'
+                    size='md'
+                    max={40}
+                    min={1}
+                    step={1}
+                    mb={20}
+                    {...form.getInputProps("pee")}
+                  />
+                </Group>
+                <Group spacing={5}>
+                  <NumberInput
+                    placeholder='Number of times pet poop'
+                    label='Poop'
+                    size='md'
+                    max={40}
+                    min={1}
+                    step={1}
+                    mb={20}
+                    {...form.getInputProps("poop")}
+                  />
+                </Group>
+                <Checkbox
+                  label='Food'
+                  size='md'
+                  mb={20}
+                  {...form.getInputProps(`food`, {
+                    type: "checkbox",
+                  })}
+                />
+                <Checkbox
+                  label='Water'
+                  size='md'
+                  mb={20}
+                  {...form.getInputProps(`water`, {
+                    type: "checkbox",
+                  })}
+                />
+                <Checkbox
+                  label='Medicine'
+                  size='md'
+                  mb={20}
+                  {...form.getInputProps(`medicine`, {
+                    type: "checkbox",
+                  })}
+                />
+                <Textarea
+                  placeholder='Walk notes'
+                  label='Walk notes'
+                  size='md'
+                  autosize
+                  minRows={2}
+                  {...form.getInputProps(`walknotes`)}
+                  mb={50}
+                />
+                <Group position='center' mt={30}>
+                  <Button type='submit' size='lg'>
+                    End Walk
+                  </Button>
+                </Group>
+              </form>
+            </Box>
+          </Grid.Col>
+        </Grid>
+      </Container>
+    </>
+  )
 }
